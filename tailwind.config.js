9ca422e1dca3e02e1dceb114384571e959a413d9
Raham Kalhoro <rahamkalhoro@gmail.com>
2026-03-29 11:17:34 +0500

    updated the html file path.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
